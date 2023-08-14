71c9a0f19b4203c7c90c8acc03a344138df7e05e
bansari <bansariradadiya57@gmail.com>
2023-08-14 12:46:34 +0530

    new

---
+++ a1.java
@@ -0,0 +1,11 @@
+
+public class a1 {
+    public static void main(String[] args) {
+        String s="1.1.1.1";
+        char str[]=s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println( s.replace
+                    (".", "[.]"));
+        }}
+}
+
